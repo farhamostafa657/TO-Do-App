5d1f1d368636c855ef5589d888c61e518ca8a62b
Your Name <farhamostafa657@gmail.com>
2025-01-31 17:28:17 +0400

    handle completed button

--- to-do-app/src/app/todo-wrapper/todo-list/card/card.component.ts
+++ to-do-app/src/app/todo-wrapper/todo-list/card/card.component.ts
@@ -1,17 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
 export class CardComponent {
+  textDecore: string = 'none';
+  completed: boolean = false;
   @Input() eachTask: any;
 
   @Output() deletedItem = new EventEmitter<string>();
 
   handleDelete(item: string) {
     this.deletedItem.emit(item);
+  }
+  handleText() {
+    this.completed = !this.completed;
+    this.textDecore = this.completed ? 'line-through' : 'none';
   }
 }
